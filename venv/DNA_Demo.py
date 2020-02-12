f97b941e7bcd834cab7ace154354a23de466f1df
patrstep <none>
2020-02-12 15:41:07 -0500

    Added extra function troubleshoot some token issue

--- venv/DNA_Demo.py
+++ venv/DNA_Demo.py
@@ -1,11 +1,17 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import getpass
-from static_config import *
+from Config import *
 
 def auth_token():
     req = requests.post(LOGONURL, auth=HTTPBasicAuth(USERNAME,PASSWORD), verify=False)
     token = req.json()['Token']
-    print(token)
+    return token
 
-auth_token()
+def get_network_devices():
+    hdr = {'x-auth-token': auth_token(), 'content-type': 'application/json'}
+    print(hdr)
+    req = requests.get(NETWORKDEVICELISTURL, headers=hdr, verify=False)
+    print(req.json())
+
+get_network_devices()
