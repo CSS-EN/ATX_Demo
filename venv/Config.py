dc71df07b3785aab90253e6c89b1445ad248a0c9
patrstep <none>
2020-02-13 17:10:49 -0500

    Added comment, demo

--- venv/Config.py
+++ venv/Config.py
@@ -2,7 +2,8 @@
 ###will be DNAC box specific. For more information about your DNAC API URL details please navigate to Platform ->
 ###Developer Toolkit -> APIs
 
-USERNAME = ""
-PASSWORD = ""
-LOGONURL = ""
-NETWORKDEVICELISTURL = ""
+USERNAME = "Demo"
+PASSWORD = "C1sco12345!"
+LOGONURL = "https://172.18.217.230/dna/system/api/v1/auth/token"
+NETWORKDEVICELISTURL = "https://172.18.217.230/dna/intent/api/v1/network-device"
+DEVICEINTERFACE = "	https://172.18.217.230/dna/intent/api/v1/interface"
--- venv/DNA_Demo.py
+++ venv/DNA_Demo.py
@@ -32,5 +32,24 @@ def print_device_list():
                          device['softwareVersion'],
                          device['role'], uptime))
 
+def print_interface_info(interface_info):
+    print("{0:42}{1:17}{2:12}{3:18}{4:17}{5:10}{6:15}".
+          format("portName", "vlanId", "portMode", "portType", "duplex", "status", "lastUpdated"))
+    for int in interface_info['response']:
+        print("{0:42}{1:10}{2:12}{3:18}{4:17}{5:10}{6:15}".
+              format(str(int['portName']),
+                     str(int['vlanId']),
+                     str(int['portMode']),
+                     str(int['portType']),
+                     str(int['duplex']),
+                     str(int['status']),
+                     str(int['lastUpdated'])))
+
+def get_device_interface(device_id):
+    hdr = {'x-auth-token': auth_token(), 'content-type': 'application/json'}
+    querystring = {"macAddress": device_id}
 
 print_device_list()
+print_interface_info() 
+
+#Change
