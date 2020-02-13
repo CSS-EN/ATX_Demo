12468eefd5dcfc1bea94ab9de6a8900a52bc0840
patrstep <none>
2020-02-13 11:18:55 -0500

    Added an extra function from DevNet code to print network devices of demo DNAC box

--- venv/DNA_Demo.py
+++ venv/DNA_Demo.py
@@ -10,8 +10,27 @@ def auth_token():
 
 def get_network_devices():
     hdr = {'x-auth-token': auth_token(), 'content-type': 'application/json'}
-    print(hdr)
     req = requests.get(NETWORKDEVICELISTURL, headers=hdr, verify=False)
-    print(req.json())
+    return req.json()
 
-get_network_devices()
+def print_device_list():
+    device_json = get_network_devices()
+    print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
+          format("hostname", "mgmt IP", "serial","platformId", "SW Version", "role", "Uptime"))
+    for device in device_json['response']:
+        uptime = "N/A" if device['upTime'] is None else device['upTime']
+        if device['serialNumber'] is not None and "," in device['serialNumber']:
+            serialPlatformList = zip(device['serialNumber'].split(","), device['platformId'].split(","))
+        else:
+            serialPlatformList = [(device['serialNumber'], device['platformId'])]
+        for (serialNumber, platformId) in serialPlatformList:
+            print("{0:42}{1:17}{2:12}{3:18}{4:12}{5:16}{6:15}".
+                  format(device['hostname'],
+                         device['managementIpAddress'],
+                         serialNumber,
+                         platformId,
+                         device['softwareVersion'],
+                         device['role'], uptime))
+
+
+print_device_list()
