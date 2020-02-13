eb73717ed3998f381413938da0ffd1421765d98a
patrstep <none>
2020-02-13 11:28:14 -0500

    Added comment

--- venv/Config.py
+++ venv/Config.py
@@ -1,3 +1,7 @@
+###This information needs to be filled out with the proper credentials before the program functions will work. The URLs
+###will be DNAC box specific. For more information about your DNAC API URL details please navigate to Platform ->
+###Developer Toolkit -> APIs
+
 USERNAME = ""
 PASSWORD = ""
 LOGONURL = ""
