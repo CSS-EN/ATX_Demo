6091097cb55f996bada6e67f9f5695164beabcbe
patrstep <none>
2020-02-19 10:29:46 -0500

    Added comment, demo

--- venv/Config.py
+++ venv/Config.py
@@ -2,8 +2,8 @@
 ###will be DNAC box specific. For more information about your DNAC API URL details please navigate to Platform ->
 ###Developer Toolkit -> APIs
 
-USERNAME = "Demo"
-PASSWORD = "C1sco12345!"
+USERNAME = ""
+PASSWORD = ""
 LOGONURL = "https://172.18.217.230/dna/system/api/v1/auth/token"
 NETWORKDEVICELISTURL = "https://172.18.217.230/dna/intent/api/v1/network-device"
 DEVICEINTERFACE = "	https://172.18.217.230/dna/intent/api/v1/interface"
